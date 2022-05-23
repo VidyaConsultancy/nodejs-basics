0a7e1693343a7cbcde3142612eeb8de7b1d9f9be
Arpit <arpit_jain@live.com>
2022-05-23 11:29:42 +0530

    feat: init basic routing in nodejs server

---
+++ Theory.md
@@ -0,0 +1,9 @@
+### HTTP Request
+- Body
+- Headers
+- Request info - URL Method HTTP version
+
+### HTTP Response
+- Body
+- Headers
+- Response info - HTTP version | HTTP status code | HTTP status message
--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const http = require("http");
 
 // request => req. request is an object of type IncomingMessage
 // response => res. response is an object of type ServerResponse
-const server = http.createServer((request, response) => {
+const requestHandler = (request, response) => {
+  const method = request.method;
+  const url = request.url;
+
+  if (method === "GET" && url === "/users") {
+    response.write("Users list");
+    response.end();
+  } else {
     response.write("Welcome! to the Nodejs server.");
     response.end();
-});
+  }
+};
+
+const server = http.createServer();
+
+server.on("request", requestHandler);
 
 server.listen(4000, "127.0.0.1", () => {
-    console.log(`Nodejs server is running at http://127.0.0.1:4000`)
-})
+  console.log(`Nodejs server is running at http://127.0.0.1:4000`);
+});
