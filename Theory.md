64680bf7a8658a4533f4abb320c7223253341ce4
Arpit <arpit_jain@live.com>
2022-05-23 15:24:26 +0530

    feat: req-res headers req body

--- Theory.md
+++ Theory.md
@@ -3,6 +3,16 @@
 - Headers
 - Request info - URL Method HTTP version
 
+### Request data
+- Url query params ?key1=value1&key2=value2
+- Url path params /users/:userId where :userId is dynamic part
+- Request Headers - creating custom headers, prefereably using x- as prefix for those headers
+- Body
+    - form-data
+    - x-www-form-urlencoded
+    - raw
+        - JSON
+
 ### HTTP Response
 - Body
 - Headers
--- server.js
+++ server.js
@@ -2,20 +2,68 @@ const http = require("http");
 
 // request => req. request is an object of type IncomingMessage
 // response => res. response is an object of type ServerResponse
+/**
+ *
+ * @param {IncomingMessage} request
+ * @param {ServerResponse} response
+ */
 const requestHandler = (request, response) => {
   const method = request.method;
   const url = request.url;
+  const headers = request.headers;
+  console.log(headers);
+  const rawData = [];
+  let data;
 
-  if (method === "GET" && url === "/users") {
-    response.write("Users list");
-    response.end();
-  } else {
-    response.write("Welcome! to the Nodejs server.");
-    response.end();
+  request.on("data", (chunk) => {
+    rawData.push(chunk);
+  });
+
+  request.on("end", () => {
+    data = Buffer.concat(rawData);
+    console.log(data.toString());
+  })
+
+  switch (url) {
+    case "/users":
+      switch (method) {
+        case "GET":
+          response.write("Users list");
+          response.end();
+          break;
+        case "POST":
+          response.statusCode = 201;
+          response.statusMessage = "Created";
+          response.setHeader("Content-Type", "application/json");
+          response.write("User created");
+          response.end();
+          break;
+        case "PUT":
+          response.write("Users updated");
+          response.end();
+          break;
+        case "PATCH":
+          response.write("Users patched");
+          response.end();
+          break;
+        case "DELETE":
+          response.write("Users deleted");
+          response.end();
+          break;
+        default:
+          response.write("Invalid request method");
+          response.end();
+          break;
+      }
+      break;
+    default:
+      response.write("Welcome! to the Nodejs server.");
+      response.end();
+      break;
   }
 };
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {});
 
 server.on("request", requestHandler);
 
